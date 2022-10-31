bfe65ac25a27d1b4543d1ebdcdbccafdf20a316a
Blue Jessen <canealisblue@gmail.com>
2022-10-31 14:29:22 -0700

    Add function to detail component

--- components/detailView/detailView.js
+++ components/detailView/detailView.js
@@ -1,11 +1,22 @@
 import styles from './detailView.module.scss'
+import { useEffect } from 'react'
+import { randomCocktailCall } from '../../utils/apiCalls'
 import Image from 'next/image'
 
 export default function detailView() {
+
+  const []
+
+  useEffect(() => {
+    randomCocktailCall()
+    .then(info => console.log(info))
+
+  }, [])
+
   return(
-    <div>
-      <h1></h1>
-      <p></p>
+    <div className={styles.detailBox}>
+      <h1>hello</h1>
+      <p>Why wont this show stuff uhg</p>
       <ul></ul>
     </div>
   )
