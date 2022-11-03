cbc22e0ac23cec545ec99a1e7059ec56d36859ed
Blue Jessen <canealisblue@gmail.com>
2022-11-02 22:52:56 -0700

    Add files for form and prompt component

---
+++ components/promptBox/promptBox.js
@@ -0,0 +1,8 @@
+import styles from './promptBox.module.scss'
+
+
+const PromptBox = () => {
+  
+}
+
+export default PromptBox
---
+++ pages/drinkPreference/form.js
@@ -0,0 +1,14 @@
+import styles from './form.module.scss'
+import { useState } from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function form() {
+  return(
+    <form>
+      <h1>Let's get some preferences</h1> 
+    </form>
+  )
+}
