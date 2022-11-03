83a60e8a95b4b3e64b4a1fed7a3e1013bb5c526f
Blue Jessen <canealisblue@gmail.com>
2022-11-02 23:19:21 -0700

    Fix syntax for checkbox input

--- components/promptBox/promptBox.js
+++ components/promptBox/promptBox.js
@@ -1,8 +1,21 @@
 import styles from './promptBox.module.scss'
 
 
-const PromptBox = () => {
-  
+export const PromptBox = ({prompt, options, handleClick}) => {
+  console.log(prompt)
+  return(
+    <div>
+      <h1>{prompt}</h1>
+      {options.map((option) => {
+        return (<label>
+        <input
+        name={option.name}
+        type={"checkbox"}
+        checked={option.checked}
+        onChange={(e) => handleClick()}
+        />{option.name}
+        </label>)
+      })}
+    </div>
+  )
 }
-
-export default PromptBox
--- pages/drinkPreference/form.js
+++ pages/drinkPreference/form.js
@@ -1,14 +1,20 @@
 import styles from './form.module.scss'
 import { useState } from 'react'
+import { PromptBox } from '../../components/promptBox/promptBox'
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 
 
 export default function form() {
+  const handleChange = (e) => {
+    console.log("hi",e)
+  }
+
   return(
     <form>
-      <h1>Let's get some preferences</h1> 
+      <h1>Let's get some preferences</h1>
+      <PromptBox prompt={"does this work?"} options={[{name: "yes", checked: true},{name:"no", checked:false}]} handleClick={handleChange} key={1}/>
     </form>
   )
 }
