dc170bbd2cffca58d1aad88e13c164cce6034a52
Blue Jessen <canealisblue@gmail.com>
2022-11-10 12:45:20 -0800

    Add flavors js file for cleaner calling and adding to flavor info

---
+++ utils/flavors.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const flavors = {
+  Grapefruit: {state:false},
+  "Lime": {state: false},
+  Strawberry:{state: false},
+  Lemon:{state: false},
+  "Chocolate":{state: false},
+  "Coffee Liqueur":{state: false},
+  "Apple juice":{state: false},
+  "Peach Schnapps":{state: false},
+  "Ginger Ale":{state: false},
+  "Cranberry juice":{state: false},
+  "Tomato juice":{state: false},
+  "Raspberry syrup":{state: false},
+  "Almond Liqueur":{state: false},
+  "Orange Liqueur":{state: false},
+  "Pineapple juice":{state: false},
+  "Mint":{state: false},
+  "Banana Liqueur":{state: false},
+  Cream: {state: false},
+  "Cherry":{state: false},
+  "Coconut Cream": {state: false},
+  "Sweet and Sour": {state: false},
+  "Tobasco sauce":{state: false}
+}
