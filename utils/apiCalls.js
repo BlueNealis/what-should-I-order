6107156dfac7367322cfa33ac4f7cfe86d3c9ea6
Blue Jessen <canealisblue@gmail.com>
2022-11-13 14:26:35 -0800

    Add new api call to file with curl format

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 export const randomCocktailCall = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v2/${process.env.NEXT_PUBLIC_API_KEY}/random.php`)
+  fetch("http://api-cocktails.herokuapp.com/api/v1/cocktails?name=ginger", {
+    headers: {
+      Authorization: `Token token=${process.env.NEXT_PUBLIC_NEW_KEY}`
+    }
+  })
   .then((response) => response.json())
+  .then((info) => console.log(info))
 }
 
 export const ingredientCocktailCall = (data) => {
